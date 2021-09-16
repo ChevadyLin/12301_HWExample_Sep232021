9d8628d76f2fdef5989e216db4ff78832845f3e9
ChevadyLin <72721482+ChevadyLin@users.noreply.github.com>
2021-09-16 19:22:16 -0400

    Add files via upload

---
+++ HWExamples_Sep232021.ino.ino
@@ -0,0 +1,44 @@
+// C++ code
+//
+
+int PIR = 2;
+unsigned long timer1;
+unsigned long timer2;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  pinMode(PIR, INPUT);
+  for(int i = 3; i <= 4; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop()
+{
+  // When PIR sensor is triggered
+  if(digitalRead(PIR)){
+    // User random() to generate a random number between 3 and 4 (included)
+    long dice = random(3, 5);
+    
+    // I use for loop here because I want to light up multiple leds.
+    // If you only want to light up the one corresponding to the value of dice,
+    // you don't have to use for loop
+    for(int i = 3; i <= dice; i++)
+    {
+      digitalWrite(i, HIGH);
+    }
+    
+    // Use delay to let leds shine for 3 seconds
+    delay(3000);
+    
+    // Dim all shining leds
+    for(int i = 3; i <= dice; i++)
+    {
+      digitalWrite(i, LOW);
+    }
+  }
+}
